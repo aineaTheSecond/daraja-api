71b2b33f7344e3f79191a55d9acbe3fdb4b0c0c3
Ainea Wabwoba <aineah28@gmail.com>
2020-10-17 07:37:20 +0300

    initial commit

---
+++ timestamp.js
@@ -0,0 +1,20 @@
+let date = new Date();
+let timestamp =
+  date.getFullYear() +
+  "" +
+  "0" +
+  date.getMonth() +
+  "" +
+  "" +
+  date.getDate() +
+  "" +
+  "0" +
+  date.getHours() +
+  "" +
+  "" +
+  date.getMinutes() +
+  "" +
+  "" +
+  date.getSeconds();
+
+alert(timestamp);
